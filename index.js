fe6ec873a4ed3ba258bd89e72c374b22b880829a
Masaya <macbeth0129will@gmail.com>
2025-01-21 22:44:49 +0900

    add an endpoint for search

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ app.post('/signup', async (req, res) => {
             .select()
         if (error) {
             console.error('Error:', error);
-            res.status(500).json({ error: 'Server error' });
+            return res.status(500).json({ error: 'Server error' });
         }
         if (data) {
             console.log(data);
-            res.status(201).json({ message: 'Success!', user: data }); 
+            return res.status(201).json({ message: 'Success!', user: data }); 
         }
     } catch (error) {
         console.error('Unexpected Error:', error);
@@ -47,18 +47,18 @@ app.post('/login', async (req, res) => {
             .eq('email', email)
         if (error) {
             console.error('Error:', error);
-            res.status(500).json({ error: 'Server error' });
+            return res.status(500).json({ error: 'Server error' });
         }
         if (data) {
             console.log(data);
             const match = await bcrypt.compare(password, data[0].password);
             if (match) {
-                res.status(200).json({ message: 'Success', data: data[0] });
+                return res.status(200).json({ message: 'Success', data: data[0] });
             } else {
-                res.status(401).json({ error: 'Invalid credentials' });
+                return res.status(401).json({ error: 'Invalid credentials' });
             }
         } else {
-            res.status(401).json({ error: 'User not found' });
+            return res.status(401).json({ error: 'User not found' });
         }
     } catch (error) {
         console.error('Unexpected Error:', error);
@@ -84,19 +84,21 @@ app.post('/recipe/mylist', async (req, res) => {
                 image,
                 description,
                 time,
+                public, 
                 date,
                 label(id, recipe_id, name),
                 material(id, recipe_id, name, quantity),
                 process(id, recipe_id, step, name)
             `)
-            .eq('account_id', account_id);
+            .eq('account_id', account_id)
+            .order('date', { ascending: false });
         if (error) {
             console.error('Error:', error);
-            res.status(500).json({ error: 'Failed to fetch recipes' });
+            return res.status(500).json({ error: 'Failed to fetch recipes' });
         }
         if (data && data.length > 0) {
             console.log(data);
-            res.status(200).json({ message: 'Success!', recipe: data }); 
+            return res.status(200).json({ message: 'Success!', recipe: data }); 
         } else {
             return res.status(404).json({ error: 'No recipes found' });
         }
@@ -108,7 +110,7 @@ app.post('/recipe/mylist', async (req, res) => {
 
 
 app.post('/recipe/add', async (req, res) => {
-    const {accountId, title, image, time, description, material, process, label} = req.body;
+    const {public_private, accountId, title, image, time, description, material, process, label} = req.body;
     let recipeId = 0;
     if (!accountId) {
         return res.status(400).json({ error: 'Account ID is required' });
@@ -118,11 +120,11 @@ app.post('/recipe/add', async (req, res) => {
         // Insert into recipe table
         const { data: recipe_data, error: recipe_error } = await supabase
             .from('recipe')
-            .insert({ account_id: accountId, name: title, image: image, description: description, time: time })
+            .insert({ public: public_private, account_id: accountId, name: title, image: image, description: description, time: time })
             .select()
         if (recipe_error) {
             console.error('Error:', recipe_error);
-            res.status(500).json({ error: 'Server error' });
+            return res.status(500).json({ error: 'Server error' });
         }
         if (recipe_data) {
             recipeId = recipe_data[0].id;
@@ -136,7 +138,7 @@ app.post('/recipe/add', async (req, res) => {
                 .select()
             if (material_error) {
                 console.error('Error:', material_error);
-                res.status(500).json({ error: 'Server error' });
+                return res.status(500).json({ error: 'Server error' });
             }
         }
 
@@ -148,7 +150,7 @@ app.post('/recipe/add', async (req, res) => {
                 .select()
             if (label_error) {
                 console.error('Error:', label_error);
-                res.status(500).json({ error: 'Server error' });
+                return res.status(500).json({ error: 'Server error' });
             }
         }
 
@@ -160,17 +162,112 @@ app.post('/recipe/add', async (req, res) => {
                 .select()
             if (process_error) {
                 console.error('Error:', process_error);
-                res.status(500).json({ error: 'Server error' });
+                return res.status(500).json({ error: 'Server error' });
             }
         }
 
-        res.status(201).json({ message: 'Success!', recipe: recipe_data }); 
+        return res.status(201).json({ message: 'Success!', recipe: recipe_data }); 
 
     } catch (error) {
         console.error('Unexpected Error:', error);
         return res.status(500).json({ error: 'Unexpected server error' });
     }
 });
+
+app.delete('/recipe/single', async (req, res) => {
+    const {recipe_id} = req.body;
+
+    if (!recipe_id) {
+        return res.status(400).json({ error: 'Account ID is required' });
+    }
+    try {
+        const { data, error } = await supabase
+            .from('recipe')
+            .delete()
+            .eq('id', recipe_id)
+            .select()  
+
+        if (error) {
+            console.error('Error:', error);
+            res.status(500).json({ error: 'Server error' });
+        }
+        if (data) {
+            console.log(data);
+            res.status(201).json({ message: 'Success!', data: data }); 
+        }
+    } catch (error) {
+        console.error('Unexpected Error:', error);
+        return res.status(500).json({ error: 'Unexpected server error' });
+    }
+});
+
+app.post('/recipe/mylist/search', async (req, res) => {
+    const {account_id, keyword} = req.body;
+    console.log(account_id);
+    if (!account_id) {
+        return res.status(400).json({ error: 'Account ID is required' });
+    }
+
+    try {
+
+        let query = supabase
+            .from('recipe')
+            .select(`
+                id,
+                account_id,
+                name,
+                image,
+                description,
+                time, 
+                public, 
+                date,
+                label(id, recipe_id, name),
+                material(id, recipe_id, name, quantity),
+                process(id, recipe_id, step, name)
+            `)
+            .eq('account_id', account_id)
+            .order('date', { ascending: false });
+        if (keyword) {
+            // query = query.eq('name', keyword);
+            query = query.ilike('name', `%${keyword}%`);
+            // query = query.eq('label.name', keyword);
+            // query = query.or(`name.eq.${keyword},label.name.eq.${keyword}`);
+            // query = query.or(`name.ilike.%${keyword}%,label.name.ilike.%${keyword}%`);
+            // query = query.or(`name.eq.%${keyword}%`, `label.name.eq.%${keyword}%`);
+
+        }
+        const { data, error } = await query;
+            // .from('recipe')
+            // .select(`
+            //     id,
+            //     account_id,
+            //     name,
+            //     image,
+            //     description,
+            //     time,
+            //     date,
+            //     label(id, recipe_id, name),
+            //     material(id, recipe_id, name, quantity),
+            //     process(id, recipe_id, step, name)
+            // `)
+            // .eq('account_id', account_id)
+            // .eq('name', keyword);
+        if (error) {
+            console.error('Error:', error);
+            return res.status(500).json({ error: 'Failed to fetch recipes' });
+        }
+        if (data && data.length > 0) {
+            console.log(data);
+            return res.status(200).json({ message: 'Success!', recipe: data }); 
+        } else {
+            return res.status(404).json({ error: 'No recipes found' });
+        }
+    } catch (error) {
+        console.error('Unexpected Error:', error);
+        return res.status(500).json({ error: 'Unexpected server error' });
+    }
+});
+
 
 const PORT = 3001;
 app.listen(PORT, '0.0.0.0', () => {
